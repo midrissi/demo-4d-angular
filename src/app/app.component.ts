d35c0e63c5533e232e7f6902a6731b53b71e0814
Mohamed IDRISSI <med.idrissi@outlook.com>
2019-11-07 11:35:32 +0100

    chore: add build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RemoteService } from './shared/4d.service';
 import { Catalog } from 'wakanda-client';
 import { IUser } from './shared/remote.interfaces';
+import { MatPaginator, PageEvent } from '@angular/material';
 
 const PAGE_SIZE = 20;
 
@@ -14,6 +15,8 @@ export class AppComponent implements OnInit{
   private ds: Catalog;
   public users: IUser[] = [];
   public displayedColumns: string[] = ['id', 'firstname', 'lastname'];
+  public pageSize = 40;
+  public length = 0;
 
   constructor(private readonly service: RemoteService) {}
 
@@ -22,11 +25,17 @@ export class AppComponent implements OnInit{
     this.refresh({});
   }
 
+  onPageChange(ev: PageEvent) {
+    this.pageSize = ev.pageSize;
+    this.refresh({ page: ev.pageIndex });
+  }
+
   async refresh({ page = 0 }) {
-    const { entities } = await this.ds.User.query<IUser>({
-      start: page*PAGE_SIZE,
-      pageSize: PAGE_SIZE
+    const { entities, _count } = await this.ds.User.query<IUser>({
+      start: page*this.pageSize,
+      pageSize: this.pageSize,
     });
     this.users = entities;
+    this.length = _count;
   }
 }
